84f5ddd56700b72fc6f5d21e18fcc2f2c6359aff
IC Tech <148203290+ivancroce@users.noreply.github.com>
2025-08-01 15:56:11 +0100

    add last part of user auth

--- src/main/java/ivancroce/event_platform/controllers/UserController.java
+++ src/main/java/ivancroce/event_platform/controllers/UserController.java
@@ -4,6 +4,8 @@ import ivancroce.event_platform.entities.User;
 import ivancroce.event_platform.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,8 +17,17 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    // ========================================== /ME ENDPOINTS ==========================================
+
+    @GetMapping("/me")
+    public User getOwnProfile(@AuthenticationPrincipal User authenticatedUser) {
+        return authenticatedUser;
+    }
+    
+    // ========================================== ADMIN ENDPOINTS ==========================================
+
     @GetMapping
-    //  @PreAuthorize("hasAuthority('ADMIN')")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public Page<User> getUsers(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
