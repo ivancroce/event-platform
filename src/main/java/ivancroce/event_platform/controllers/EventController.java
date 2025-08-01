3e67026e9a885411d40be0427cd3b02c8ca66d22
IC Tech <148203290+ivancroce@users.noreply.github.com>
2025-08-01 17:04:28 +0100

    add events endpoints

--- src/main/java/ivancroce/event_platform/controllers/EventController.java
+++ src/main/java/ivancroce/event_platform/controllers/EventController.java
@@ -5,11 +5,14 @@ import ivancroce.event_platform.entities.User;
 import ivancroce.event_platform.payloads.EventDTO;
 import ivancroce.event_platform.services.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/events")
@@ -24,5 +27,34 @@ public class EventController {
     public Event createEvent(@RequestBody @Validated EventDTO payload,
                              @AuthenticationPrincipal User currentUser) {
         return eventService.createEvent(payload, currentUser);
+    }
+
+    @GetMapping
+    public Page<Event> getAllEvents(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "eventDate") String sortBy) {
+        return eventService.findAllEvents(page, size, sortBy);
+    }
+
+    @GetMapping("/{eventId}")
+    public Event getEventById(@PathVariable UUID eventId) {
+        return eventService.findById(eventId);
+    }
+
+    @PutMapping("/{eventId}")
+    @PreAuthorize("hasAnyAuthority('ORGANIZER', 'ADMIN')")
+    public Event updateEvent(@PathVariable UUID eventId,
+                             @RequestBody @Validated EventDTO payload,
+                             @AuthenticationPrincipal User currentUser) {
+        return eventService.updateEvent(eventId, payload, currentUser);
+    }
+
+    @DeleteMapping("/{eventId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasAnyAuthority('ORGANIZER', 'ADMIN')")
+    public void deleteEvent(@PathVariable UUID eventId,
+                            @AuthenticationPrincipal User currentUser) {
+        eventService.deleteEvent(eventId, currentUser);
     }
 }
--- src/main/java/ivancroce/event_platform/services/EventService.java
+++ src/main/java/ivancroce/event_platform/services/EventService.java
@@ -2,10 +2,19 @@ package ivancroce.event_platform.services;
 
 import ivancroce.event_platform.entities.Event;
 import ivancroce.event_platform.entities.User;
+import ivancroce.event_platform.enums.Role;
+import ivancroce.event_platform.exceptions.NotFoundException;
 import ivancroce.event_platform.payloads.EventDTO;
 import ivancroce.event_platform.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class EventService {
@@ -24,5 +33,49 @@ public class EventService {
         newEvent.setOrganizer(organizer);
 
         return eventRepository.save(newEvent);
+    }
+
+    public Page<Event> findAllEvents(int page, int size, String sortBy) {
+        if (size > 50) size = 50;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+        return eventRepository.findAll(pageable);
+    }
+
+    public Event findById(UUID eventId) {
+        return eventRepository.findById(eventId)
+                .orElseThrow(() -> new NotFoundException("Event with ID " + eventId + " not found."));
+    }
+
+    public Event updateEvent(UUID eventId, EventDTO payload, User currentUser) {
+        Event foundEvent = this.findById(eventId);
+
+        boolean isOwner = foundEvent.getOrganizer().getId().equals(currentUser.getId());
+        boolean isAdmin = currentUser.getRole() == Role.ADMIN;
+
+        if (isOwner || isAdmin) {
+
+            foundEvent.setTitle(payload.title());
+            foundEvent.setDescription(payload.description());
+            foundEvent.setEventDate(payload.eventDate());
+            foundEvent.setLocation(payload.location());
+            foundEvent.setTotalSeats(payload.totalSeats());
+
+            return eventRepository.save(foundEvent);
+        } else {
+            throw new AuthorizationDeniedException("You are not authorized to modify this event.");
+        }
+    }
+
+    public void deleteEvent(UUID eventId, User currentUser) {
+        Event foundEvent = this.findById(eventId);
+
+        boolean isOwner = foundEvent.getOrganizer().getId().equals(currentUser.getId());
+        boolean isAdmin = currentUser.getRole() == Role.ADMIN;
+
+        if (isOwner || isAdmin) {
+            eventRepository.delete(foundEvent);
+        } else {
+            throw new AuthorizationDeniedException("You are not authorized to delete this event.");
+        }
     }
 }
